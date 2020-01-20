735b8e789884ff4dfcc4a9b042f0e2c2b2e5e538
furkancesur <frkncesur@gmail.com>
2020-01-20 03:46:06 +0300

    End of the story

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //Check Off Specific todos by clicking
 
-$("li").click(function (){
+$("ul").on("click", "li",function (){
     /*if($(this).css("color") === "rgb(128, 128, 128)"){
         $(this).css({
             color : "black",
@@ -13,4 +13,24 @@ $("li").click(function (){
         });
     }*/
     $(this).toggleClass("completed");
+});
+
+// Click on X button to delete item
+$("ul").on("click", "span",function (event) {
+    $(this).parent().fadeOut(500,function (){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+// Entering text
+$("input[type='text']").keypress(function (event) {
+   if(event.which === 13){
+       var todoText = $(this).val();
+       $(this).val("");
+       $("ul").append("<li><span><i class=\'fa fa-trash\'></i> </span>"+ todoText +"</li>");
+   }
+});
+
+$(".fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
 });
